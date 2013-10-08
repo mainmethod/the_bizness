1b7add8359d8217a456da371b045e1300e4b1233
Rob Farrell <codeninja@local.local>
2013-10-07 20:16:18 -0400

    inital app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheBizness::Application.config.secret_token = '6e1d8c0720bc9f72d3a5b7a9f9b55970c60ea5266234e5a6e724b56966fe679bc2c8ac2250708ee1e029e027c93a3bda3d1f8e078b2ca7b9895b9f658a832511'
