a6920880273d488f6cc4842c4bc161e5520f8a24
Ben Walker <bwalker@asheavenue.com>
2013-10-20 22:35:37 -0400

    adding layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   
   protect_from_forgery
+  
+  layout :layout
+
+private
+  
+  def layout
+    if (is_a?(Devise::SessionsController) || is_a?(Devise::RegistrationsController))
+      "login"
+    else
+      "application"
+    end
+  end
+ 
 end
