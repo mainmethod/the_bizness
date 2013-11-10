3464f2352a9f7b08b54dfff21caf193fcf05237d
Coder <rob.c.farrell@gmail.com>
2013-11-09 22:53:48 -0500

    adds comment and post user links, and redirects to user profile for show if current user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,6 @@ class CommentsController < ApplicationController
     @comment.parent_id = params[:post_id]
     @comment.user = current_user
     @comment.save
-    respond_to do |format|
-      format.js
-    end
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    redirect_to profile_path if @user==current_user
   end
   
   def follow
