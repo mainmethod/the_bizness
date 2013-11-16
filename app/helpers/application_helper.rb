e84d017d3733fdd9786cfc5becf93285eb7e93c5
Ben Walker <bwalker@asheavenue.com>
2013-11-16 12:18:07 -0500

    nav update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def nav_link(link_text, link_path, icon)
+  
+  active_class = current_page?(link_path) ? 'active ' : ''
+  icon = content_tag(:span, :class => ('icon icon-' << icon)) do end;
+  link_text = icon << link_text
+  
+  content_tag(:li, :class => active_class) do
+    link_to link_text, link_path
+  end
+end
+
 end
