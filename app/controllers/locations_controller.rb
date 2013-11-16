630fe5916a2065592779b34dd7b6ce22bc352e40
Coder <rob.c.farrell@gmail.com>
2013-11-16 18:16:37 -0500

    some numbers

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,6 +7,6 @@ class LocationsController < ApplicationController
   def nearby
     @location = request.location.state ? current_user.location : request.location
     @proximity = params[:proximity] ? params[:proximity] : 200
-    @locations = @location.nearbys(@proximity)
+    @locations = @location.nearbys(@proximity).unshift(@location)
   end
 end
