c0cb067b4bccbd976697aa5666955a0300a26342
Ben Walker <bwalker@asheavenue.com>
2014-06-21 17:55:16 -0400

    style location search

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,9 @@
 class LocationsController < ApplicationController
   
   def show
-    @location = Location.includes(:users).find(params[:id])
+    @location  = Location.includes(:users).find(params[:id])
+    @proximity = params[:proximity] ? params[:proximity] : 200
+    @locations = @location.nearbys(@proximity).unshift(@location)
   end
   
   def nearby
