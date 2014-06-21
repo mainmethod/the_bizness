042c321a0809efff41a799feffa77b12baac4806
Rob Farrell <coder@work.local>
2014-06-21 17:35:40 -0400

    some locations to the search autocomplete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
   
   def search
     @users = params[:term] ? User.except_me(current_user).where("first_name like ? or last_name like ?","%#{params[:term]}%","%#{params[:term]}%").limit(7) : [];
+    @locations = params[:term] ? Location.where("city like ? or state like ?","%#{params[:term]}%","%#{params[:term]}%").limit(7) : [];
     respond_to do |format|
       format.js
     end
