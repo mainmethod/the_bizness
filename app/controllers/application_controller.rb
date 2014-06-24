9eaf1b566dbd0d32f9b9602698535968bc108472
Rob Farrell <coder@work.local>
2014-06-24 12:41:48 -0400

    devise nonsense

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   protect_from_forgery
   
   layout :layout
+  
+protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name, :about, :location_attributes => [:zipcode])}
+  end
 
 private
   
